9ae1a78461d56826099bbcf4a19e01cedcfbd81f
Gaurav Gautam <gautam1168@gmail.com>
2026-08-02 14:02:27 +0530

    Chapter 16

--- main.c
+++ main.c
@@ -1,34 +1,68 @@
-#include "chunk.h"
-#include "debug.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "vm.h"
 
+static char * readFile(const char *filePath) {
+  FILE *file = fopen(filePath, "rb");
+  if (file == NULL) {
+    printf("Could not open file at \"%s\".\n", filePath);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char *source = (char *)malloc(sizeof(char) * (fileSize + 1));
+  if (source == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", filePath);
+    exit(74);
+  }
+
+  size_t bytesRead = fread(source, sizeof(char), fileSize, file);
+  source[fileSize] = '\0';
+  fclose(file);
+  if (bytesRead < fileSize) {
+    printf("Couldn't read whole file\n");
+    exit(74);
+  }
+
+  return source;
+}
+
+static void repl() {
+  char line[1024];
+  for (;;) {
+    printf("> ");
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+    interpret(line);
+  }
+}
+
+static void runFile(const char *filePath) {
+  char *source = readFile(filePath);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR) exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char *argv[]) {
-  Chunk chunk;
-  initChunk(&chunk);
   initVM();
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-
-  writeChunk(&chunk, OP_NEGATE, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  // disassembleChunk(&chunk, "test chunk");
-  interpret(&chunk);
-
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
--- vm.c
+++ vm.c
@@ -2,6 +2,7 @@
 #include "debug.h"
 #include "value.h"
 #include "vm.h"
+#include "compiler.h"
 #include <stdio.h>
 
 VM vm;
@@ -35,61 +36,60 @@ uint8_t readByte() {
   return result;
 }
 
-static InterpretResult run() {
-#define READ_BYTE() *(vm.ip++)
-#define READ_CONSTANT() vm.chunk->constants.values[READ_BYTE()]
-#define BINARY_OP(op) do { \
-  Value b = pop(); \
-  Value a = pop(); \
-  push(a op b);\
-} while(false)
+// static InterpretResult run() {
+// #define READ_BYTE() *(vm.ip++)
+// #define READ_CONSTANT() vm.chunk->constants.values[READ_BYTE()]
+// #define BINARY_OP(op) do { \
+//   Value b = pop(); \
+//   Value a = pop(); \
+//   push(a op b);\
+// } while(false)
 
-  for (;;) {
-#ifdef DEBUG_TRACE_EXECUTION
-    printf("           ");
-    for (Value *slot = vm.stack; slot < vm.stackTop; ++slot) {
-      printf("[");
-      printValue(*slot);
-      printf("]");
-    }
-    printf("\n");
-    int offset = (int)(vm.ip - vm.chunk->code);
-    disassembleInstruction(vm.chunk, offset);
-#endif
-    uint8_t instruction = READ_BYTE();
-    switch (instruction) {
-      case OP_RETURN:
-      {
-          Value result = pop();
-          printValue(result);
-          printf("\n");
-          return INTERPRET_OK;
-          break;
-      }
-      case OP_ADD: BINARY_OP(+); break;
-      case OP_SUBTRACT: BINARY_OP(-); break;
-      case OP_MULTIPLY: BINARY_OP(*); break;
-      case OP_DIVIDE: BINARY_OP(/); break;
-      case OP_NEGATE:
-      {
-        push(-pop());
-        break;
-      }
-      case OP_CONSTANT:
-      {
-        Value constant = READ_CONSTANT();
-        push(constant);
-        break;
-      }
-    }
-  }
-#undef READ_BYTE
-#undef READ_CONSTANT
-#undef BINARY_OP
-}
+//   for (;;) {
+// #ifdef DEBUG_TRACE_EXECUTION
+//     printf("           ");
+//     for (Value *slot = vm.stack; slot < vm.stackTop; ++slot) {
+//       printf("[");
+//       printValue(*slot);
+//       printf("]");
+//     }
+//     printf("\n");
+//     int offset = (int)(vm.ip - vm.chunk->code);
+//     disassembleInstruction(vm.chunk, offset);
+// #endif
+//     uint8_t instruction = READ_BYTE();
+//     switch (instruction) {
+//       case OP_RETURN:
+//       {
+//           Value result = pop();
+//           printValue(result);
+//           printf("\n");
+//           return INTERPRET_OK;
+//           break;
+//       }
+//       case OP_ADD: BINARY_OP(+); break;
+//       case OP_SUBTRACT: BINARY_OP(-); break;
+//       case OP_MULTIPLY: BINARY_OP(*); break;
+//       case OP_DIVIDE: BINARY_OP(/); break;
+//       case OP_NEGATE:
+//       {
+//         push(-pop());
+//         break;
+//       }
+//       case OP_CONSTANT:
+//       {
+//         Value constant = READ_CONSTANT();
+//         push(constant);
+//         break;
+//       }
+//     }
+//   }
+// #undef READ_BYTE
+// #undef READ_CONSTANT
+// #undef BINARY_OP
+// }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
