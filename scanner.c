3857a1cf1f7e1c03d7bb326606eb8ce0e0d9d321
Gaurav Gautam <gautam1168@gmail.com>
2026-08-06 05:11:12 +0530

    Remove test code that was added to let the scanner function during development'

--- scanner.c
+++ scanner.c
@@ -16,19 +16,19 @@ void initScanner(const char *source) {
   scanner.current = source;
   scanner.line = 1;
 
-  int line = -1;
-  for (;;) {
-    Token token = scanToken();
-    if (token.line != line) {
-      printf("%4d ", token.line);
-      line = token.line;
-    } else {
-      printf("   | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+  // int line = -1;
+  // for (;;) {
+  //   Token token = scanToken();
+  //   if (token.line != line) {
+  //     printf("%4d ", token.line);
+  //     line = token.line;
+  //   } else {
+  //     printf("   | ");
+  //   }
+  //   printf("%2d '%.*s'\n", token.type, token.length, token.start);
 
-    if (token.type == TOKEN_EOF) break;
-  }
+  //   if (token.type == TOKEN_EOF) break;
+  // }
 }
 
 static bool isAtEnd() {
